a6ffb4b0983db13175ba4efcdcf8da6cf4d21461
DannyIsa <danieli@wix.com>
2025-07-20 12:52:44 +0300

    s

--- MyContainer.hpp
+++ MyContainer.hpp
@@ -6,55 +6,58 @@
 #include <algorithm>
 #include <functional>
 
-namespace ariel {
+namespace ariel
+{
 
-template<typename T>
-class MyContainer {
-private:
-    std::vector<T> elements;
-    
-    std::vector<T> ascending_copy;
-    std::vector<T> descending_copy;
-    std::vector<T> side_cross_copy;
-    std::vector<T> reverse_copy;
-    std::vector<T> middle_out_copy;
-
-public:
-    class Iterator {
+    template <typename T>
+    class MyContainer
+    {
     private:
-        typename std::vector<T>::iterator current;
-        
+        std::vector<T> elements;
+
+        std::vector<T> ascending_copy;
+        std::vector<T> descending_copy;
+        std::vector<T> side_cross_copy;
+        std::vector<T> reverse_copy;
+        std::vector<T> middle_out_copy;
+
     public:
-        Iterator(typename std::vector<T>::iterator it);
-        T& operator*();
-        Iterator& operator++();
-        bool operator!=(const Iterator& other) const;
+        class Iterator
+        {
+        private:
+            typename std::vector<T>::iterator current;
+
+        public:
+            Iterator(typename std::vector<T>::iterator it);
+            T &operator*();
+            Iterator &operator++();
+            bool operator!=(const Iterator &other) const;
+        };
+
+        void addElement(const T &element);
+        void remove(const T &element);
+        size_t size() const;
+
+        Iterator begin_ascending_order();
+        Iterator end_ascending_order();
+
+        Iterator begin_descending_order();
+        Iterator end_descending_order();
+
+        Iterator begin_side_cross_order();
+        Iterator end_side_cross_order();
+
+        Iterator begin_reverse_order();
+        Iterator end_reverse_order();
+
+        Iterator begin_order();
+        Iterator end_order();
+
+        Iterator begin_middle_out_order();
+        Iterator end_middle_out_order();
     };
 
-    void addElement(const T& element);
-    void remove(const T& element);
-    size_t size() const;
-
-    Iterator begin_ascending_order();
-    Iterator end_ascending_order();
-    
-    Iterator begin_descending_order();
-    Iterator end_descending_order();
-    
-    Iterator begin_side_cross_order();
-    Iterator end_side_cross_order();
-    
-    Iterator begin_reverse_order();
-    Iterator end_reverse_order();
-    
-    Iterator begin_order();
-    Iterator end_order();
-    
-    Iterator begin_middle_out_order();
-    Iterator end_middle_out_order();
-};
-
-template<typename T>
-std::ostream& operator<<(std::ostream& os, MyContainer<T>& container);
+    template <typename T>
+    std::ostream &operator<<(std::ostream &os, MyContainer<T> &container);
 
 }
